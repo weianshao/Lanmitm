0f740007183bd4c52c364c1b44a423ac16655a98
oinux <ssun125@163.com>
2014-12-09 11:14:25 +0800

    添加http深度代理，将代理分为简单代理和深度代理，简单代理只做数据流转发。深度代理可以对http内容进行修改，目前添加对utf-8、gbk和gb2312的网页的支持。

--- src/com/oinux/lanmitm/proxy/HttpProxy.java
+++ src/com/oinux/lanmitm/proxy/HttpProxy.java
@@ -1,34 +1,39 @@
 package com.oinux.lanmitm.proxy;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.http.HttpRequest;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.RequestLine;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
-import org.apache.http.impl.DefaultHttpRequestFactory;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicRequestLine;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.util.EntityUtils;
 
-import android.provider.CalendarContract.Instances;
 import android.util.Log;
 
 import com.oinux.lanmitm.AppContext;
@@ -41,6 +46,8 @@ import com.oinux.lanmitm.util.RequestParser;
  */
 public class HttpProxy extends Thread {
 
+	public static final String HTTP_POST = "POST";
+	public static final String HTTP_GET = "GET";
 	private final static int HTTP_SERVER_PORT = 80;
 	public static final int HTTP_PROXY_PORT = 8080;
 	private static final int BACKLOG = 255;
@@ -99,7 +106,7 @@ public class HttpProxy extends Thread {
 			executor = Executors.newCachedThreadPool();
 			while (!stop) {
 				Socket client = mServerSocket.accept();
-				executor.execute(new DealThread(client, mOnRequestListener));
+				executor.execute(new SimpleDealThread(client, mOnRequestListener));
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -119,37 +126,222 @@ public class HttpProxy extends Thread {
 
 	class DeepDealThread extends DealThread {
 
+		private String decode = "UTF-8";
+
 		public DeepDealThread(Socket client, OnRequestListener onRequestListener) {
 			super(client, onRequestListener);
 		}
 
-		@Override
-		public void dealResponse(){
-			Log.v("deal", "deal");
+		private String readResponse(HttpResponse response) {
+			HttpEntity entity = response.getEntity();
+			InputStream inputStream = null;
+			try {
+				inputStream = entity.getContent();
+				ByteArrayOutputStream content = new ByteArrayOutputStream();
+
+				boolean hasCharset = false;
+				Header header = response.getFirstHeader("Content-Type");
+				if (header != null) {
+					String ct = header.getValue().toLowerCase(
+							Locale.getDefault());
+					if (ct.contains("charset=utf-8")) {
+						decode = "UTF-8";
+						hasCharset = true;
+					} else if (ct.contains("charset=gbk")) {
+						decode = "GBK";
+						hasCharset = true;
+					} else if (ct.contains("charset=gb2312")) {
+						decode = "GB2312";
+						hasCharset = true;
+					}
+				}
+
+				int readBytes = 0;
+				byte[] sBuffer = new byte[512];
+				while ((readBytes = inputStream.read(sBuffer)) != -1) {
+					content.write(sBuffer, 0, readBytes);
+				}
+				String result = new String(content.toByteArray(), decode);
+				if (!hasCharset) {
+					String charset = decode;
+					Pattern p = Pattern.compile("content=\".*charset=(.*?)\"");
+					Matcher m = p.matcher(result);
+					if (m.find()) {
+						boolean isUnknown = false;
+						charset = m.group(1);
+						if (charset.equalsIgnoreCase("utf-8")) {
+							charset = "UTF-8";
+						} else if (charset.equalsIgnoreCase("gbk")) {
+							charset = "GBK";
+						} else if (charset.equalsIgnoreCase("gb2312")) {
+							charset = "GB2312";
+						} else {
+							isUnknown = true;
+						}
+						if (!isUnknown) {
+							if (!charset.equalsIgnoreCase(decode)) {
+								decode = charset;
+								result = new String(content.toByteArray(),
+										charset);
+							}
+						}
+					}
+				}
+				return result;
+			} catch (IllegalStateException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return null;
 		}
+
+		@Override
+		public void run() {
+			byte[] buffer = new byte[1024];
+			int read = 0;
+			InputStream inputStream = null;
+
+			try {
+				inputStream = clientSocket.getInputStream();
+				final String clientIp = clientSocket.getInetAddress()
+						.getHostAddress();
+
+				if ((read = inputStream.read(buffer, 0, 1024)) >= 0) {
+					ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(
+							buffer, 0, read);
+					BufferedReader bReader = new BufferedReader(
+							new InputStreamReader(byteArrayInputStream));
+					String line = null, serverName = null, path = null, method = null, url = null;
+					boolean found = false, foundPath = false, ok = true;
+					ArrayList<String> headers = new ArrayList<String>();
+					StringBuilder builder = new StringBuilder();
+
+					HttpClient httpClient = new DefaultHttpClient();
+					HttpUriRequest request = null;
+					List<Header> httpHeaders = new ArrayList<Header>();
+
+					while ((line = bReader.readLine()) != null) {
+						headers.add(line);
+						builder.append(line + "\r\n");
+
+						if (line.contains("HTTP/1.1"))
+							line = line.replace("HTTP/1.1", "HTTP/1.0");
+						else if (line.indexOf(':') != -1) {
+							String[] split = line.split(":", 2);
+							String header = split[0].trim(), value = split[1]
+									.trim();
+							if (header.equals("Accept-Encoding"))
+								value = "identity";
+							else if (header.equals("Connection"))
+								value = "close";
+							else if (header.equals("If-Modified-Since")
+									|| header.equals("Cache-Control"))
+								header = null;
+							if (header != null)
+								httpHeaders.add(new BasicHeader(header, value));
+						}
+						if (!found && line.contains("Host")) {
+							serverName = line.substring(5).trim();
+							found = true;
+						}
+						if (!foundPath) {
+							path = line;
+							foundPath = true;
+							Matcher m = PATTERN.matcher(path);
+							if (m.find())
+								ok = false;
+							method = line.substring(0, line.indexOf(' '));
+							url = line.substring(line.indexOf(' ') + 1,
+									line.lastIndexOf(' '));
+						}
+					}
+					builder.append("\r\n");
+
+					if (serverName != null) {
+						writer = new BufferedOutputStream(
+								clientSocket.getOutputStream());
+
+						String serverIp = InetAddress.getByName(serverName)
+								.getHostAddress();
+						if (onRequestListener != null && ok) {
+							onRequestListener.onRequest(clientIp, serverName,
+									serverIp, path, headers);
+						}
+						
+						if (ok) {
+							if (HTTP_GET.equals(method)) {
+								request = new HttpGet("http://" + serverName
+										+ url);
+							} else if (HTTP_POST.equals(method)) {
+								request = new HttpPost("http://" + serverName
+										+ url);
+							}
+							if (request != null) {
+								request.setHeaders(httpHeaders
+										.toArray(new Header[httpHeaders.size()]));
+								HttpResponse response = httpClient
+										.execute(request);
+								if (response != null
+										&& response.getStatusLine()
+												.getStatusCode() == HttpStatus.SC_OK) {
+									String data = null;
+									data = readResponse(response);
+									writer.write(data.getBytes(decode));
+								}
+							}
+						} else {
+							String req = builder.toString();
+							serverSocket = new Socket(serverName,
+									HTTP_SERVER_PORT);
+							serverSocket.setSoTimeout(1000);
+
+							serverReader = serverSocket.getInputStream();
+							serverWriter = serverSocket.getOutputStream();
+
+							serverWriter.write(req.getBytes());
+							serverWriter.flush();
+
+							byte[] buff = new byte[1024];
+							int len = -1;
+							while ((len = serverReader.read(buff, 0, 1024)) >= 0) {
+								writer.write(buff, 0, len);
+								writer.flush();
+							}
+						}
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (inputStream != null) {
+						inputStream.close();
+					}
+					if (writer != null) {
+						writer.close();
+					}
+					if (serverReader != null) {
+						serverReader.close();
+					}
+					if (serverWriter != null) {
+						serverWriter.close();
+					}
+					if (clientSocket != null)
+						clientSocket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
 
-	class DealThread extends Thread {
+	class SimpleDealThread extends DealThread {
 
-		private OnRequestListener onRequestListener;
-		private Socket clientSocket;
-		private Socket serverSocket;
-		private BufferedOutputStream writer = null;
-		private InputStream serverReader = null;
-		private OutputStream serverWriter = null;
-
-		public DealThread(Socket client, OnRequestListener onRequestListener) {
-			this.onRequestListener = onRequestListener;
-			this.clientSocket = client;
-		}
-
-		public void dealResponse() throws IOException { 
-			byte[] buff = new byte[1024];
-			int len = -1;
-			while ((len = serverReader.read(buff, 0, 1024)) >= 0) {
-				writer.write(buff, 0, len);
-				writer.flush();
-			}
+		public SimpleDealThread(Socket client,
+				OnRequestListener onRequestListener) {
+			super(client, onRequestListener);
 		}
 
 		@Override
@@ -209,7 +401,12 @@ public class HttpProxy extends Thread {
 						serverWriter.write(request.getBytes());
 						serverWriter.flush();
 
-						dealResponse();
+						byte[] buff = new byte[1024];
+						int len = -1;
+						while ((len = serverReader.read(buff, 0, 1024)) >= 0) {
+							writer.write(buff, 0, len);
+							writer.flush();
+						}
 					}
 				}
 			} catch (IOException e) {
@@ -220,11 +417,6 @@ public class HttpProxy extends Thread {
 						inputStream.close();
 					}
 					if (writer != null) {
-						if (inject != null) {
-							writer.flush();
-							writer.write(inject.getBytes());
-							writer.flush();
-						}
 						writer.close();
 					}
 					if (serverReader != null) {
@@ -241,6 +433,21 @@ public class HttpProxy extends Thread {
 					e.printStackTrace();
 				}
 			}
+		}
+	}
+
+	class DealThread extends Thread {
+
+		protected OnRequestListener onRequestListener;
+		protected Socket clientSocket;
+		protected Socket serverSocket;
+		protected BufferedOutputStream writer = null;
+		protected InputStream serverReader = null;
+		protected OutputStream serverWriter = null;
+
+		public DealThread(Socket client, OnRequestListener onRequestListener) {
+			this.onRequestListener = onRequestListener;
+			this.clientSocket = client;
 		}
 	}
 
